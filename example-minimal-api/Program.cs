6c12efbd4f6158a81555006e2ab27a868c6d9a12
Carlos Marques <carlos.marques89@gmail.com>
2023-07-26 16:31:46 -0400

    Adding minimal api examples

--- example-minimal-api/Program.cs
+++ example-minimal-api/Program.cs
@@ -1,25 +1,83 @@
-var builder = WebApplication.CreateBuilder(args);
+using example_minimal_api.Domain.Interfaces;
+using example_minimal_api.Domain.Models;
+using example_minimal_api.Domain.Services;
+using example_minimal_api.Infrastructure.Data;
+using Microsoft.OpenApi.Models;
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSingleton<ITaskRepository, TaskRepository>();
+builder.Services.AddSingleton<ITaskService, TaskService>();
+
+// Add this section to enable Swagger support
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Todo API", Version = "v1"});
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-app.UseStaticFiles();
-
 app.UseRouting();
 
-app.UseAuthorization();
+app.MapGet("/todos", async (ITaskService service) =>
+{
+    var todos = await service.GetAllAsync();
+    return Results.Ok(todos);
+});
 
-app.MapRazorPages();
+app.MapGet("/todos/{id}", async (int id, ITaskService service) =>
+{
+    var todo = await service.GetByIdAsync(id);
+    if (todo == null)
+        return Results.NotFound();
 
+    return Results.Ok(todo);
+});
+
+app.MapPost("/todos", async (Todo todo, ITaskService service) =>
+{
+    if (string.IsNullOrEmpty(todo.Title))
+        return Results.BadRequest("Title must not be empty");
+
+    var addedTodo = await service.AddAsync(todo);
+    return Results.Created($"/todos/{addedTodo.Id}", addedTodo);
+});
+
+app.MapPut("/todos/{id}", async (int id, Todo todo, ITaskService service) =>
+{
+    if (string.IsNullOrEmpty(todo.Title))
+        return Results.BadRequest("Title must not be empty");
+
+    var existingTodo = await service.GetByIdAsync(id);
+    if (existingTodo == null)
+        return Results.NotFound();
+
+    todo.Id = id;
+    await service.UpdateAsync(todo);
+    return Results.Ok(todo);
+});
+
+app.MapDelete("/todos/{id}", async (int id, ITaskService service) =>
+{
+    var deleted = await service.DeleteAsync(id);
+    if (!deleted)
+        return Results.NotFound();
+
+    return Results.NoContent();
+});
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API V1");
+    c.RoutePrefix = string.Empty;
+});
 app.Run();
