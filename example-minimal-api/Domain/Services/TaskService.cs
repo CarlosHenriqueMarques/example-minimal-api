026118f21921cc637c46f547a59fc4e20c26162a
Carlos Marques <carlos.marques89@gmail.com>
2023-07-26 16:06:49 -0400

    Implementing service class

--- example-minimal-api/Domain/Services/TaskService.cs
+++ example-minimal-api/Domain/Services/TaskService.cs
@@ -5,28 +5,33 @@ namespace example_minimal_api.Domain.Services;
 
 public class TaskService : ITaskService
 {
+    private readonly ITaskRepository _taskRepository;
+
+    public TaskService(ITaskRepository taskRepository) { 
+        _taskRepository = taskRepository;
+    }
     public Task<Todo> AddAsync(Todo task)
     {
-        throw new NotImplementedException();
+        return _taskRepository.AddAsync(task);
     }
 
     public Task<bool> DeleteAsync(int id)
     {
-        throw new NotImplementedException();
+        return _taskRepository.DeleteAsync(id);
     }
 
     public Task<IEnumerable<Todo>> GetAllAsync()
     {
-        throw new NotImplementedException();
+        return _taskRepository.GetAllAsync();
     }
 
     public Task<Todo> GetByIdAsync(int id)
     {
-        throw new NotImplementedException();
+        return _taskRepository.GetByIdAsync(id);
     }
 
     public Task<Todo> UpdateAsync(Todo task)
     {
-        throw new NotImplementedException();
+        return _taskRepository.UpdateAsync(task);
     }
 }
